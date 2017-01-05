e1bd89edb9d3814aacabde66daa3f6af2480cf2a
frederik <frederik.slos@gmail.com>
2017-01-05 20:38:55 +0100

    update homepage for multiple alarms

--- wakeuplight_java_helpers/src/LightServlet.java
+++ wakeuplight_java_helpers/src/LightServlet.java
@@ -4,7 +4,7 @@
 /* This is an emulator of the Wake-up Light device to facilitate development of the web page. */
 
 import java.io.IOException;
-import java.io.PrintWriter;
+//import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,56 +19,35 @@ import javax.servlet.http.HttpServletResponse;
 public class LightServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	enum Mode { INIT, ENABLED, DISABLED, ACTIVE }
+	enum AppMode { INIT, ACTIVE }
+	enum AlarmMode { UNSET, SET, ACTIVE }
 	
 	int initCount = 0;
-	Mode mode = Mode.INIT;
+	AppMode mode = AppMode.INIT;
+	AlarmMode alarm = AlarmMode.UNSET;
 	int duty_manual = 100;
-	int alarm_hour = 0;
+	int alarm_hour = 9;
 	int alarm_minute = 0;
 	int duration = 30;
 	
+	int alarm_count = 1;
+	
+	static int  MAX_ALARMS = 8;
+	
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		initCount++;
-		if(initCount == 2) mode = Mode.ENABLED;
+		if(initCount == 2) mode = AppMode.ACTIVE;
+		
+		String str;
+	    String alarmlist = "";
+	    String statuslist;
+		
+		String alarms = request.getParameter("alarms");
 		
 		String button = request.getParameter("btn");
 		if(button != null) {
 			switch(button) {
-			case "btn_light_full":
-				duty_manual = 100;
-				break;
-			case "btn_light_more":
-				duty_manual += 5;
-				if(duty_manual > 100) duty_manual = 100;
-				break;
-			case "btn_light_less":
-				duty_manual -= 5;
-				if(duty_manual < 0) duty_manual = 0;
-				break;
-			case "btn_light_off":
-				duty_manual = 0;
-				break;
-			case "btn_alarm_hh_inc":
-				alarm_hour = (alarm_hour + 1) % 24;
-				break; 
-			case "btn_alarm_hh_dec":
-				alarm_hour = (alarm_hour + 23) % 24;
-				break; 
-			case "btn_alarm_mm_inc":
-				alarm_minute = alarm_minute + 5;
-				if(alarm_minute >= 60) {
-					alarm_minute -= 60;
-					alarm_hour = (alarm_hour + 1) % 24;
-				}
-				break; 
-			case "btn_alarm_mm_dec":
-				alarm_minute = alarm_minute - 5;
-				if(alarm_minute < 0) {
-					alarm_minute += 60;
-					alarm_hour = (alarm_hour + 23) % 24;
-				}
-				break; 
 			case "btn_alarm_slower":
 				duration += 5;
 				if(duration >= 60*3) duration = 60*3;
@@ -77,34 +56,74 @@ public class LightServlet extends HttpServlet {
 				duration -= 5;
 				if(duration < 0) duration = 0;
 				break;
-			case "btn_alarm_toggle":
-				switch(mode) {
-				case INIT: break;
-				case ENABLED: mode = Mode.DISABLED; break;
-				case DISABLED: mode = Mode.ENABLED; break;
-				case ACTIVE: mode = Mode.ENABLED; break;
+			case "btn_alarm_dismis":
+				if (alarm == AlarmMode.ACTIVE) {
+					alarm = AlarmMode.UNSET;
+				}
+				break;
+			case "btn_alarm_add":
+				if (alarm_count <= MAX_ALARMS) alarm_count += 1;
+				if (alarms == null) alarms = "";
+				break;
+			default:
+				if (button.contains("btn_delete-")){
+					alarm_count -= 1;
+					if (alarms == null) alarms = "";
+				}
+				break;
+			}
+		}
+		String input = request.getParameter("inp");
+		if(input != null) {
+			String value = request.getParameter("val");
+			switch(input){
+			case "usr_time":
+				// todo fix set per alarm
+				String id = request.getParameter("id");
+				if(id != null){
+					if(value != null && value.contains(":")) {
+						String[] value_ar = value.split(":");
+						alarm_hour = Integer.parseInt(value_ar[0]);
+						alarm_minute = Integer.parseInt(value_ar[1]);
+					}
+				}
+				break;
+			case "enable":
+				if(value != null) {
+					// todo fix set per alarm
+					if (value.contentEquals("true")) {
+						alarm = AlarmMode.SET; 
+					}
+					else alarm = AlarmMode.UNSET;
 				}
 				break;
 			}
 		}
 		
-		int alarm_to_hour = alarm_hour;
-		int alarm_to_minute = alarm_minute;
-		alarm_to_minute += duration;
-		alarm_to_hour += alarm_to_minute / 60;
-		alarm_to_hour = alarm_to_hour % 24;
-		alarm_to_minute = alarm_to_minute % 60;
-				
-		DateFormat timeFormat = new SimpleDateFormat("HH:mm");
-		DateFormat dateFormat = new SimpleDateFormat("d MMMMM yyyy");
+		DateFormat hourFormat = new SimpleDateFormat("h");
+		DateFormat minuteFormat = new SimpleDateFormat("m");
+		DateFormat dayFormat = new SimpleDateFormat("d");
+		DateFormat monthFormat = new SimpleDateFormat("MMMMM");
+		DateFormat yearFormat = new SimpleDateFormat("yyyy");
 		Date date = new Date();
-		try(PrintWriter pw = new PrintWriter(response.getOutputStream())) {
-			pw.println(timeFormat.format(date));
-			pw.println(dateFormat.format(date));
-			pw.println(String.format("%d:%02d", alarm_hour, alarm_minute));
-			pw.println(String.format("%d:%02d", alarm_to_hour, alarm_to_minute));
-			pw.println(mode.name());
+		for (int i=0; i<alarm_count; i++){
+			if (i!=0) alarmlist += ",";
+			alarmlist += "{\"id\":\"" + String.format("%d", i) + "\",\"hour\":8, \"minute\":0,\"enable\": true,\"repeat\":true,\"mon\":true,\"tue\":true,\"wed\":true,\"thu\":true,\"fri\":true,\"sat\":true,\"sun\":true}";
 		}
-	}
+		alarmlist = "\"alarms\":[" + alarmlist + "]";
+	    statuslist = "\"status\": \""+ mode.name() + "\",\"alarm\":\""+ alarm.name() + 
+	    		"\",\"day\": "+ dayFormat.format(date) + ",\"month\":\""+ monthFormat.format(date) + "\",\"year\":"+ yearFormat.format(date) +
+	    		", \"hour\": " + hourFormat.format(date) + ", \"minute\": " + minuteFormat.format(date) +
+	    		", \"next_day\": \"Tomorrow\",\"next_hour\": " + hourFormat.format(date)+ 1 + ", \"next_minute\": " + minuteFormat.format(date) +
+	    		", \"to_hour\": " + hourFormat.format(date)+ 1 + ", \"to_minute\": " + minuteFormat.format(date) ;
+		
+		if (alarms != null) {
+			str = "{" + alarmlist + "}";
+		} else {
+			str = "{" + statuslist + "}";
+		}
 
+		response.getOutputStream().print(str);
+		response.setContentType("application/json");
+	}
 }
