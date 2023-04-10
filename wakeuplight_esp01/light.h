c0c77d1dcc81a409a5164809d569066edae78702
frederik <frederik.slos@gmail.com>
2023-04-10 15:24:16 +0200

    avoid warning: unused parameter

--- wakeuplight_esp01/light.h
+++ wakeuplight_esp01/light.h
@@ -7,7 +7,7 @@
 class Light {
 public:
   Light() {};
-  virtual void increaseTo(float scale) {};
+  virtual void increaseTo(float /* scale */) {};
   virtual void setup() {};
   virtual void loop() {};
 };
